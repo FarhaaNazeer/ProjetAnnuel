00b10e3b829d61ec009f98b45a50046b62eda04b
Sonny <sonny.luscap@gmail.com>
2021-05-06 19:20:59 +0200

    Commit Dashboard html

---
+++ www/Dashboard.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html> 
+<html>
+   <head>
+      <title> Dashboard </title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="style.css">
+   </head>
+   <body>
+      <nav id="sidenav">
+         <h1> Gestion de Contenus </h1>
+         <ul>
+            <li><a href="#"> Page </a></li>
+            <li><a href="#"> Extension </a></li>
+            <li><a href="#"> Medias </a></li>
+         </ul>
+         <h1>  Apparence </h1>
+         <ul>
+            <li><a href="#"> Menus</a></li>
+            <li><a href="#"> Thèmes</a></li>
+         </ul>
+         <h1>  Administration </h1>
+         <ul>
+            <li><a href="#"> Utilisateurs </a></li>
+            <li><a href="#"> Réservations </a></li>
+            <li><a href="#"> Réglages </a></li>
+         </ul>
+      </nav>
+      <nav id="topnav">
+         <h1> Tableau de bord </h1>
+         <ul>
+            <li><a href="#"> Tableau de board </a></li>
+            <li><a href="#"> Aide </a>
+            <li>
+            <li><a href="#"> Mes sites </a>
+            <li> 
+         </ul>
+      </nav>
+      <section id="kpi">
+         <h1> Indidcateur de performance </h1>
+         <div class="bloc"></div>
+         <div class="bloc"></div>
+         <div class="bloc"></div>
+      </section>
+      <section id="welcoming-indication">
+         <h1> Configurez votre site </h1>
+         <!-- Prévoir progress bar -->
+         <ul>
+            <li> Créez votre de domaine </li>
+            <li><a href="#">Créer</a></li>
+         </ul>
+         <ul>
+            <li> Ajoutez votre première </li>
+            <li><a href="#">Ajouter</a></lI>
+         </ul>
+         <ul>
+            <li> Publiez votre site </li>
+            <li><a href="#">Publier </a></li>
+         </ul>
+      </section>
+      <section id="Suggestions">
+         <div class="widgets">
+            <h2> Google Analytics </h2>
+         </div>
+         <a href="#">Analyser vos données</a> 
+         <div class="widgets">
+            <h2> Google Maps </h2>
+         </div>
+         <a href="#">Indiquer votre localisation</a> 
+         <div class="widgets">
+            <h2> Traduction de pages </h2>
+         </div>
+         <a href="#">Proposer une traduction</a> 
+      </section>
+      <section id="last-reservations">
+         <h1> Liste des 5 dernières réservation </h1>
+         <!-- Tableau généré en PHP --> 
+      </section>
+      <section id="last-registrations">
+         <h1> Liste des 5 dernières commandes </h1>
+         <!-- Tableau généré en PHP --> 
+      </section>
+   </body>
+</html>
+
