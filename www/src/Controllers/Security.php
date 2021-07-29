3870c75dbd5bc3b67d700be6605a186feb326d53
Wendy Afrim <wendy.afrim2@gmail.com>
2021-07-29 12:46:13 +0200

    Setting du fichier .env + restriction accès server

--- www/src/Controllers/Security.php
+++ www/src/Controllers/Security.php
@@ -1,14 +1,20 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
 use App\Core\View;
+use Dotenv\Dotenv;
 
-class Security {
+class Security
+{
 
-    public function login() {
+    public function login()
+    {
         // echo "page login";
         // die();
+        $dotenv = Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+        $dotenv->load();
+
         $view = new View('Security/login', 'front-template');
 
         // $view = $view->renderHtml();
